b0fd2d6db780dd6d58fa683a31b55fe8e9868a1c
Timoteo <phillipstr@gmail.com>
2017-11-24 10:24:07 -0500

    adds logging to SyllabusController

--- src/main/java/com/ako/api/SyllabusController.java
+++ src/main/java/com/ako/api/SyllabusController.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.lang.Exception;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -21,6 +23,7 @@ import com.ako.service.S3Service;
 import com.ako.service.SyllabusService;
 
 
+
 /**
  * Syllabus Controller
  * @author Tim
@@ -32,10 +35,14 @@ public class SyllabusController {
     @Autowired
     S3Service s3Service;
    
+    /* Logger */
+    private final Logger logger = LogManager.getLogger(SyllabusController.class);
+    
     
     @RequestMapping(method=RequestMethod.GET, value="download")
     public @ResponseBody void downloadSyllabus() {
         
+        logger.info("SyllabusController.downloadSyllabus called");
         s3Service.downloadFile("syllabus.pdf");
 
 	}
@@ -45,11 +52,13 @@ public class SyllabusController {
     @RequestMapping(method=RequestMethod.POST, value="upload")
     public @ResponseBody void uploadSyllabus(@RequestParam("syllabus") MultipartFile file) {
 
+        logger.info("SyllabusController.uploadSyllabus called with: " + file.getOriginalFilename());
         try {
             s3Service.uploadFile("syllabus.pdf", file);
+            logger.info("SyllabusController.uploadFile successful!");
 
         } catch (Exception ace) {
-            System.err.println("SyllabusController ==> UploadSyllabus Error");
+            logger.error("SyllabusController.UploadSyllabus Error");
         }
     }   
 }
