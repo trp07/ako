fe67da37ddc70ffef147f2bbeadbaba881e4a5e0
Vishakha Sadhwani <vishakha@Vishakhas-MacBook-Pro.local>
2017-11-26 12:22:35 -0500

    adding messages functionality

---
+++ src/main/java/com/ako/data/MessageUser.java
@@ -0,0 +1,41 @@
+package com.ako.data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class MessageUser {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+	
+	int userId;
+	int groupId;
+	int messageUserTypeId;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getGroupId() {
+		return groupId;
+	}
+	public void setGroupId(int groupId) {
+		this.groupId = groupId;
+	}
+	public MessageUserType getMessageUserTypeId() {
+		return MessageUserType.getType(this.messageUserTypeId);
+	}
+	public void setMessageUserTypeId(MessageUserType messageUserTypeId) {
+		this.messageUserTypeId = messageUserTypeId.getType();
+	}
+	public int getId() {
+		return id;
+	}
+}
