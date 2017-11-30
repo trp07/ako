6e7499c3bb7c9a5a968b500b7c934361f32809eb
noel.buruca <burucan@wireless-10-104-200-208.umd.edu>
2017-11-29 21:27:33 -0500

    Added annotations

--- src/main/java/com/ako/data/Semester.java
+++ src/main/java/com/ako/data/Semester.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
@@ -23,10 +24,12 @@ public class Semester {
 
 	private String name;
 	
+	@JsonGetter("id")
 	private int getId() {
 		return this.id;
 	}
 	
+	@JsonGetter("name")
 	private String getName() {
 		return this.name;
 	}
