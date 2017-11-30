59b87efa25c5d665db188e98b8f3a65cd7d07e59
noel.buruca <burucan@wireless-10-104-200-208.umd.edu>
2017-11-29 21:06:14 -0500

    Added Table Annotation and JsonIgnore

--- src/main/java/com/ako/data/Semester.java
+++ src/main/java/com/ako/data/Semester.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
  * Semester model representing Semester table.
@@ -11,10 +14,13 @@ import javax.persistence.Id;
  * @author noel.buruca
  */
 @Entity
+@Table(name="Semester")
+@JsonIgnoreProperties(value = {"id", "type"}, allowGetters = true)
 public class Semester {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+
 	private String name;
 	
 	private int getId() {
