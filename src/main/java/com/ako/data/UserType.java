c7ba597925f5ffd714d3cd5cc0cda26a00d96962
noel.buruca <burucan@wireless-10-104-200-208.umd.edu>
2017-11-29 21:06:32 -0500

    Added Table Annotation and JsonIgnore

--- src/main/java/com/ako/data/UserType.java
+++ src/main/java/com/ako/data/UserType.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 /**
@@ -13,10 +16,13 @@ import javax.persistence.Id;
  */
 
 @Entity
+@Table(name = "User_type")
+@JsonIgnoreProperties(value = {"id", "type"}, allowGetters = true)
 public class UserType {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+	
 	private String type;
 	
 	private int getId() {
