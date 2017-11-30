ed6abaffb942dab40d70ede94585221bcd6d516d
noel.buruca <burucan@noels-mbp.wfm.pvt>
2017-11-30 09:32:50 -0500

    Changed LocalDate to Date

--- src/main/java/com/ako/data/Course.java
+++ src/main/java/com/ako/data/Course.java
@@ -1,6 +1,6 @@
 package com.ako.data;
 
-import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -34,7 +34,7 @@ public class Course {
 	@Column(nullable = false, updatable = false)
     @Temporal(TemporalType.TIMESTAMP)
     @CreatedDate
-	private LocalDate createDate;
+	private Date createDate;
 	
 	@Column(nullable = false)
 	String shortName;
@@ -62,7 +62,7 @@ public class Course {
 		return id;
 	}
     @JsonGetter("createDate")
-	public LocalDate getCreateDate() {
+	public Date getCreateDate() {
 		return createDate;
 	}
     @JsonGetter("shortName")
